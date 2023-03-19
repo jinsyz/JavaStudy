adb233c2b6dd3a2bcd03eeeba947cb97b43af8e7
jinsy <Jinsyz95@gmail.com>
2023-03-19 15:41:35 +0900

    ch 11

---
+++ src/chap_11/_02_Catch.java
@@ -0,0 +1,24 @@
+package chap_11;
+
+public class _02_Catch {
+    public static void main(String[] args) {
+        try{
+            //System.out.println(3/0);
+//            int[] arr = new int[3];
+//            arr[5] = 100;
+            Object obj = "test";
+            System.out.println((int)obj);
+        } catch(ArithmeticException e) {
+            System.out.println("뭔가 잘못 계산 하셨네요.");
+
+        } catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("인덱스를 잘못 실행 하셨어요.");
+        } catch (ClassCastException e){
+            System.out.println("잘못된 형 변환 입니다.");
+        }catch (Exception e) {
+            System.out.println("이런 문제가 발생했어요 => " +e.getMessage());
+            e.printStackTrace();
+        }
+        System.out.println("프로그램 정상 종료");
+    }
+}
